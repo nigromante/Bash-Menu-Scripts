8ed7029a5ae54672d54d385041ca137f5ed244d0
“nigromante” <julianvidal@live.cl>
2024-08-04 14:00:50 -0400

    mplayer video normalizado

--- app/menu_mplayer.sh
+++ app/menu_mplayer.sh
@@ -5,14 +5,17 @@ function menu_mplayer() {
 
     title "Menu Mplayer"
     
-    local items=("Lista Dispositivos" "Video Normal" "Video Nocturno")
+    local items=("Lista Dispositivos" "Video 0" "Video 1" "Video 2" "Video 3" "Video 4" )
 
     select item in "${items[@]}" Volver
     do
         case $REPLY in
             1) call "mplayer/list" ;; 
-            2) call "mplayer/normal" ;; 
-            3) call "mplayer/night" ;; 
+            2) call "mplayer/video" "video0" ;; 
+            3) call "mplayer/video" "video1" ;; 
+            4) call "mplayer/video" "video2" ;; 
+            5) call "mplayer/video" "video3" ;; 
+            6) call "mplayer/video" "video4" ;; 
             $((${#items[@]}+1))) 
                 header
                 return ;;
