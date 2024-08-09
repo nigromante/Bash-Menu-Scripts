0384632bb8b73e9e27dc92410cd7ed97c64891af
“nigromante” <julianvidal@live.cl>
2024-08-08 23:45:29 -0400

    refactor pruba menu

--- scripts/key.sh
+++ scripts/key.sh
@@ -2,13 +2,16 @@
 source ../includes/ezl_imports_scripts.sh
 
 clear
+print "${ERROR}EJEMPLO 1 (usa string)${RESET}${BRx4}${BRx3}"
 opciones="uno dos tres cuatro cinco seis" 
 _menu_init "${opciones}"
 _menu "respta"
 
 print "result ::: ${ERROR}(${respta})${RESET}${BRx2}"
-print "${BRx4}"
 
+
+
+print "${ERROR}EJEMPLO 2 (usa arrego)${RESET}${BRx4}"
 
 menuItems=("alfa" "beta" "gama")
 _menu "respta2"
